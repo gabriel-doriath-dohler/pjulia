9f5da0b04e95461bf69e03ec3066a3500d618c00
Gabriel DORIATH DÖHLER <gabriel.doriath.dohler@ens.psl.eu>
2021-01-08 17:41:56 +0100

    Typo.

--- src/type.ml
+++ src/type.ml
@@ -66,7 +66,7 @@ let type1_func f =
 	end
 
 let rec type1_expr env e = match snd e with
-	(* Add the global variables to the environement. *)
+	(* Add the global variables to the environment. *)
 	| Call (_, (e1 :: b)) | Par (e1 :: b) ->
 		type1_block (type1_expr env e1) b
 	| Lval (_, Field (e1, _)) | Not e1 | Return (Some e1) | While (e1, _) ->
@@ -83,20 +83,20 @@ let rec type1_expr env e = match snd e with
 	| _ -> env
 
 and type1_else_block env eb = match snd eb with
-	(* Add the global variables to the environement. *)
+	(* Add the global variables to the environment. *)
 	| End					-> env
 	| Else b				-> type1_block env b
 	| Elseif (e, b, eb1)	-> type1_else_block (type1_block env (e :: b)) eb1
 
 and type1_block env =
-	(* Add the global variables to the environement. *)
+	(* Add the global variables to the environment. *)
 	List.fold_left type1_expr env
 
 let rec type1 env = function
 	(* Check the structure definitions.
 	Check the function definitions (but not the bodies).
 	Keep track of all the function and structure definition.
-	Add the global variables to the environement. *)
+	Add the global variables to the environment. *)
 	| Expr e :: l	-> type1 (type1_expr env e) l
 	| Func f :: l	-> type1_func f; type1 env l
 	| []			-> env
@@ -104,21 +104,17 @@ let rec type1 env = function
 (* Second part. *)
 
 let type2 env = function
-	(* Construct the local environement of each function/while loop/for loop.
+	(* Construct the local environment of each function/while loop/for loop.
 	Verify that all return instructions on a function have a type compatible with the return type.
 	Verify that all function/while loop/for loop bodies are well typed.
-	Verify that global expressions are well typed.
-	Verify that variables might be defined before being used. *)
+	Verify that global expressions are well typed. *)
 	| _ -> [] (* TODO *)
 
 (* Part 1 & part 2. *)
 
 let typing ast =
+	(* Construct the typed ast, the global environment, the local environments and type check. *)
 	let genv = type1 Env.bindings ast in
-	(*
-	TODO
 	let tast = type2 genv ast in
-	*)
-	let tast = [] in
 	genv, tast
 
